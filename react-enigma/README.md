9a78f00bed5e0002a998ac520fbd5e48f2a499e5
Kevin Conway <kevinconway@Kevins-MBP.attlocal.net>
2023-02-11 10:27:36 -0500

    Added better state object and pulled rotor settings from Wikipedia

--- react-enigma/README.md
+++ react-enigma/README.md
@@ -5,32 +5,10 @@
 2. Rotor state object: 
 
 {
-    "a": "a",
-    "b": "b",
-    "c": "c",
-    "d": "d",
-    "e": "e",
-    "f": "f",
-    "g": "g",
-    "h": "h",
-    "i": "i",
-    "j": "j",
-    "k": "k",
-    "l": "l",
-    "m": "m",
-    "n": "n",
-    "o": "o",
-    "p": "p",
-    "q": "q",
-    "r": "r",
-    "s": "s",
-    "t": "t",
-    "u": "u",
-    "v": "v",
-    "w": "w",
-    "x": "x",
-    "y": "y",
-    "z": "z"
+    name: 1,
+    wiring: [],
+    notch: 1,
+    position: 1
 }
 
 3. Rotation function
---
+++ react-enigma/src/components/Enigma/rotors.js
@@ -0,0 +1,54 @@
+const rotors = {
+    1: {
+        name: "I",
+        wiring: "EKMFLGDQVZNTOWYHXUSPAIBRCJ",
+        notch: 1,
+        position: 1
+    },
+    2: {
+        name: "II",
+        wiring: "AJDKSIRUXBLHWTMCQGZNPYFVOE",
+        notch: 1,
+        position: 1
+    },
+    3: {
+        name: "III",
+        wiring: "BDFHJLCPRTXVZNYEIWGAKMUSQO",
+        notch: 1,
+        position: 1
+    },
+    4: {
+        name: "IV",
+        wiring: "ESOVPZJAYQUIRHXLNFTGKDCMWB",
+        notch: 1,
+        position: 1
+    },
+    5: {
+        name: "V",
+        wiring: "VZBRGITYUPSDNHLXAWMJQOFECK",
+        notch: 1,
+        position: 1
+    },
+    6: {
+        name: "VI",
+        wiring: "JPGVOUMFYQBENHZRDKASXLICTW",
+        notch: 1,
+        position: 1
+    },
+    7: {
+        name: "VII",
+        wiring: "NZJHGRCXMYSWBOUFAIVLPEKQDT",
+        notch: 1,
+        position: 1
+    },
+    8: {
+        name: "VIII",
+        wiring: "FKQHTLXOCBJSPDZRAMEWNIUYGV",
+        notch: 1,
+        position: 1
+    },
+
+}
+
+export default rotors;
+
