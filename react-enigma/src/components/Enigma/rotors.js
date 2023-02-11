594072eb573b773e75d2916ea95803376362aaba
Kevin Conway <kevinconway@Kevins-MBP.attlocal.net>
2023-02-11 10:57:17 -0500

    brainstormed the processRotors function without wheel turn

--- react-enigma/src/components/Enigma/rotors.js
+++ react-enigma/src/components/Enigma/rotors.js
@@ -47,8 +47,34 @@ const rotors = {
         notch: 1,
         position: 1
     },
-
+    reflector: {
+        name: "Reflector",
+        wiring: "QYHOGNECVPUZTFDJAXWMKISRBL"
+    }
 }
 
 export default rotors;
+
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+const processRotors = (letter, settings) => {
+    const { rotorsConfig: rc, reflector } = settings
+    const { indexOf: ai } = alphabet;
+
+    // fill in the plugboard later
+
+
+    // this is how it would work if the rotors didn't move
+    const rotor1return = rc[1].wiring[(ai(letter))];
+    const rotor2return = rc[2].wiring[(ai(rotor1return))];
+    const rotor3return = rc[3].wiring[(ai(rotor2return))];
+
+    const reflectorReturn = reflector.wiring[(ai(rotor3return))];
+
+    const rotor3backwards = rc[3].wiring.indexOf(reflectorReturn);
+    const rotor2backwards = rc[2].wiring.indexOf(rotor3backwards);
+    const result = rc[1].wiring.indexOf(rotor2backwards);
+    
+    return result;
+}
 
