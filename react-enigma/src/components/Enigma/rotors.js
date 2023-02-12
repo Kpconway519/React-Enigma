0d87b91936a8bdc8d5baed27c96f8a459b1d851e
Kevin Conway <kevinconway@Kevins-MBP.attlocal.net>
2023-02-12 13:34:00 -0500

    working and tested POC as long as the rings are static

--- react-enigma/src/components/Enigma/rotors.js
+++ react-enigma/src/components/Enigma/rotors.js
@@ -1,19 +1,19 @@
 const rotors = {
     1: {
         name: "I",
-        wiring: "EKMFLGDQVZNTOWYHXUSPAIBRCJ",
+        wiring: "EKMFLGDQVZNTOWYHXUSPAIBRCJ",// A => E // W => N
         notch: 1,
         position: 1
     },
     2: {
         name: "II",
-        wiring: "AJDKSIRUXBLHWTMCQGZNPYFVOE",
+        wiring: "AJDKSIRUXBLHWTMCQGZNPYFVOE",// E => S // F => W
         notch: 1,
         position: 1
     },
     3: {
         name: "III",
-        wiring: "BDFHJLCPRTXVZNYEIWGAKMUSQO",
+        wiring: "BDFHJLCPRTXVZNYEIWGAKMUSQO",// S => G // L => F
         notch: 1,
         position: 1
     },
@@ -49,32 +49,57 @@ const rotors = {
     },
     reflector: {
         name: "Reflector",
-        wiring: "QYHOGNECVPUZTFDJAXWMKISRBL"
+        wiring: "YRUHQSLDPXNGOKMIEBFZCWVJAT" // G => L
     }
 }
 
-export default rotors;
+// export default rotors;
 
-const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const rotorsConfigTest = {
+    rotorsConfig: [
+    rotors[1],
+    rotors[2],
+    rotors[3]
+    ],
+    reflector: rotors.reflector
+}
 
-const processRotors = (letter, settings) => {
+
+const processRotors = async (letter, settings) => {
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    console.log(alphabet)
     const { rotorsConfig: rc, reflector } = settings
-    const { indexOf: ai } = alphabet;
+    // const { indexOf: ai } = alphabet;
 
     // fill in the plugboard later
 
+    const findLetterPosition = async (rotor, letter) => {
+        // console.log(alphabet.indexOf(letter), alphabet.indexOf('A'), ai('A'))
+        console.log('letter', letter)
+
+        const returnIndex = alphabet.indexOf(letter) + (rotor.position === 1 ? 0 : rotor.position - 1);
+        const rotorLength = (rotor.wiring.length - 1)
+        // console.log(rotor, letter)
+        // console.log('letter', letter, 'alphabet', alphabet)
+        // console.log('ai letter', alphabet.indexOf(letter), 'rotorposition', rotor.position)
+        // console.log('returnindex', returnIndex, 'rotorlength', rotorLength)
+        if (rotorLength < returnIndex) return (returnIndex - rotorLength) // should correct overflow but i may be off by one
+        // console.log('returning second one')
+        return returnIndex; // if all is well, return normal index.
+    };
 
     // this is how it would work if the rotors didn't move
-    const rotor1return = rc[1].wiring[(ai(letter))];
-    const rotor2return = rc[2].wiring[(ai(rotor1return))];
-    const rotor3return = rc[3].wiring[(ai(rotor2return))];
-
-    const reflectorReturn = reflector.wiring[(ai(rotor3return))];
-
-    const rotor3backwards = rc[3].wiring.indexOf(reflectorReturn);
-    const rotor2backwards = rc[2].wiring.indexOf(rotor3backwards);
-    const result = rc[1].wiring.indexOf(rotor2backwards);
     
-    return result;
+    const rotor1return = rc[0].wiring[(alphabet.indexOf(letter))];
+    var rotor2return = rc[1].wiring[(alphabet.indexOf(await rotor1return))];
+    const rotor3return = await rc[2].wiring[(alphabet.indexOf(await rotor2return))];
+
+    const reflectorReturn = reflector.wiring[(await alphabet.indexOf(rotor3return))];
+    
+    const rotor3backwards = await rc[2].wiring.indexOf(reflectorReturn);
+    const rotor2backwards = await rc[1].wiring.indexOf(alphabet[rotor3backwards]);
+    const result = await await rc[0].wiring.indexOf(alphabet[rotor2backwards]);
+    return await result;
 }
 
+processRotors("A", rotorsConfigTest)
